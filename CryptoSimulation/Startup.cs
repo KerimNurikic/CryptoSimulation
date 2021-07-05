93c0cffe0be40988298d9b83678bd0b038bb7938
KerimNurikicETF <knurikic1@etf.unsa.ba>
2021-07-05 23:45:06 +0200

    Sign-in or register using your Google account option added

--- CryptoSimulation/Startup.cs
+++ CryptoSimulation/Startup.cs
@@ -35,6 +35,14 @@ namespace CryptoSimulation
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
+                    options.ClientId = googleAuthNSection["ClientId"];
+                    options.ClientSecret = googleAuthNSection["ClientSecret"];
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
